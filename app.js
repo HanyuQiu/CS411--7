6163f2093e7f55f631e8e488e9cb0e44939b4f3a
Yu Ham Ng <144143493+FrankyNg1@users.noreply.github.com>
2023-12-05 19:19:49 -0500

    Updated app.js

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
-const apiUrl = 'https://tasty.p.rapidapi.com/recipes/auto-complete';
+const apiUrl = 'https://api.spoonacular.com/recipes/complexSearch';
 let authToken = null;
 
 
+async function login() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
+    try {
+        const response = await fetch(`${apiUrl}/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password }),
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            authToken = data.token;
+
+            // Hide login section and show recipe search section
+            document.getElementById('authSection').style.display = 'none';
+            document.getElementById('recipeSection').style.display = 'block';
+        } else {
+            console.error('Login failed');
+        }
+    } catch (error) {
+        console.error('Error during login:', error);
+    }
+}
 async function searchRecipes() {
     const searchInput = document.getElementById('searchInput').value;
 
