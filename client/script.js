a4a16519bfe51ac007ddb5d9ea3c0e7873883296
Franky Ng <franky@crc-dot1x-nat-10-239-131-238.bu.edu>
2023-12-11 19:01:38 -0500

    updated

--- client/script.js
+++ client/script.js
@@ -1,36 +1,86 @@
-function login() {
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
+const apiUrl = 'https://api.spoonacular.com/recipes/complexSearch';
+let authToken = null;
 
-    // Here you should add code to send the username and password to the server
-    // Assuming successful login
-    document.getElementById('login-form').style.display = 'none';
-    document.getElementById('ingredient-input').style.display = 'block';
-}
 
-function getRecipes() {
-    var ingredient = document.getElementById('ingredient').value;
+async function login() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
-    // Send ingredients to the back end
-    fetch('/get-recipes', {
-        method: 'POST',
-        body: JSON.stringify({ ingredient: ingredient }),
-        headers: {
-            'Content-Type': 'application/json'
+    try {
+        const response = await fetch(`${apiUrl}/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password }),
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            authToken = data.token;
+
+            // Hide login section and show recipe search section
+            document.getElementById('authSection').style.display = 'none';
+            document.getElementById('recipeSection').style.display = 'block';
+        } else {
+            console.error('Login failed');
         }
-    })
-    .then(response => response.json())
-    .then(data => {
-        displayRecipes(data.recipes);
+    } catch (error) {
+        console.error('Error during login:', error);
+    }
+}
+async function searchRecipes() {
+    const searchInput = document.getElementById('searchInput').value;
+
+    try {
+        const response = await fetch(`${apiUrl}/search?query=${searchInput}`, {
+            headers: {
+                'Authorization': `Bearer ${authToken}`,
+            },
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        } else {
+            console.error('Recipe search failed');
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+
+function displayResults(results) {
+    const resultsContainer = document.getElementById('results');
+    //clear the previous result
+    resultsContainer.innerHTML = ''; 
+
+    if (results.length === 0) {
+        resultsContainer.innerHTML = '<p>No recipes found</p>';
+        return;
+    }
+
+    results.forEach(recipe => {
+        const recipeCard = document.createElement('div');
+        recipeCard.classList.add('recipe-card');
+
+        const title = document.createElement('h2');
+        title.textContent = recipe.title;
+
+        const ingredients = document.createElement('p');
+        ingredients.textContent = `Ingredients: ${recipe.ingredients.join(', ')}`;
+
+        const instructions = document.createElement('p');
+        instructions.textContent = `Instructions: ${recipe.instructions}`;
+
+        recipeCard.appendChild(title);
+        recipeCard.appendChild(ingredients);
+        recipeCard.appendChild(instructions);
+
+        resultsContainer.appendChild(recipeCard);
     });
 }
 
-function displayRecipes(recipes) {
-    var recipesDiv = document.getElementById('recipes');
-    recipesDiv.innerHTML = '';
-    recipes.forEach(recipe => {
-        var p = document.createElement('p');
-        p.textContent = recipe;
-        recipesDiv.appendChild(p);
-    });
-}
+
+
